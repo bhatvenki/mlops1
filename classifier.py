bc4a982dd5e3011e21a6c74717c62a4e51cf2633
bhatvenki <bhatvenki@gmail.com>
2023-08-21 06:03:30 +0000

    changed the test size to 10%

--- classifier.py
+++ classifier.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 df = pd.read_csv("/content/mlops1/data/Iris.csv")
 features = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 target = 'Species'
-X_train, X_test, y_train, y_test = train_test_split(df[features],df[target],test_size=0.3,shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(df[features],df[target],test_size=0.1,shuffle=True)
 clf = DecisionTreeClassifier(criterion='entropy')
 clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
